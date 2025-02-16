93030bd3773b1d18f4a88ceac435ff33f6f79e88
Vikzdimension <vivek.lode1@gmail.com>
2025-02-16 18:36:34 +0530

    upload
    upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
-        }        
+        }
+        
         $this->app->make(TaskScheduler::class)->register(app('Illuminate\Console\Scheduling\Schedule'));    
     }
 }
